73307ac1dce73d4484e08342df50b5d3549cd041
r.s <nakayasute777@gmail.com>
2020-02-29 22:22:45 +0900

    Add Gcd

--- library_atcoder.py
+++ library_atcoder.py
@@ -1,14 +1,14 @@
 '''
 # 繰り返し二乗
-# 2のn乗を高速に計算する
+# aのn乗を高速に計算する
 '''
-def repeat_two_square( n ):
+def repeat_any_square( a,n ):
     if n == 1:
-        return 2
-    x = repeat_two_square( n//2 )
+        return a
+    x = repeat_any_square( a,n//2 )
     x *= x
     if n % 2 == 1:
-        x *= 2
+        x *= a
     return x
 
 '''
@@ -22,5 +22,5 @@ def gcd(a,b):
     ans = gcd(b,surplus)
     return ans
 
-print(repeat_two_square( 5 ))
+print(repeat_any_square( 7,7 ))
 print(gcd(3355,2379))
